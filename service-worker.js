748f4225b84bf88654fda0288e9982ff7684f433
MBishop84 <mitch.bishop@proton.me>
2025-02-04 16:21:43 +0000

    Deploying to gh-pages from @ MBishop84/GitTransformer@e170b06a880298fe4a89b24e6428f49ad064e823 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,30 +1,4 @@
 // In development, always fetch from the network and do not enable offline support.
 // This is because caching would make development more difficult (changes would not
 // be reflected on the first load after each change).
-self.addEventListener('fetch', () => { });
-
-self.addEventListener("DOMContentLoaded", (event) => {
-    let theme = localStorage.getItem('RadzenTheme');
-    if (!theme) {
-        const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
-        if (darkThemeMq.matches) {
-            theme = 'dark';
-        } else {
-            theme = 'default';
-        }
-        localStorage.setItem('RadzenTheme', theme);
-    }
-    let themeLink = document.getElementById('theme');
-    themeLink.href = `_content/Radzen.Blazor/css/${theme}.css`;
-});
-
-Blazor.addEventListener('SetAppData', {
-    bubbles: true,
-    browserEventName: "load",
-    createEventArgs: (event) => {
-        return {
-            WindowHeight: window.innerHeight,
-            WindowWidth: window.innerWidth
-        };
-    }
-});
+//self.addEventListener('fetch', () => { });
