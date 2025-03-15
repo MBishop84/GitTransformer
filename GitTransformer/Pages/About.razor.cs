e4160240de734d25b9c74b8fb45f8db2c352b49f
Mitch Bishop <Mitch.Bishop@NGIC.COM>
2025-03-15 19:49:45 -0400

    Enhance About page layout and functionality
    Significantly modified the `About.razor` file to improve layout and functionality, including the addition of a `RadzenStack` component, dynamic management of section expansion states, and a toggle button for the skills section. Updated the education section to include certificate years and enhanced the work history section with `RadzenTimelineItem` components featuring company logos and updated job titles. Added tooltips for expandable sections.
    
    Updated the `About.razor.cs` file to include necessary namespaces and logic for managing expansion states and tooltips.
    
    Added a new image file `3m.png` for the work history section, along with two additional PNG files: `donaldson.png` and `NGIC.png`.

---
+++ GitTransformer/Pages/About.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using Radzen;
+
+namespace GitTransformer.Pages;
+
+public partial class About
+{
+    [Inject] TooltipService TooltipService { get; set; } = null!;
+
+    List<bool> _isExpanded = [
+        false, false, false, false, false, false, false];
+    string _icon = "stat_minus_2";
+
+    void ShowTooltip(ElementReference elementReference, TooltipOptions options) => 
+        TooltipService.Open(elementReference, _isExpanded[0] ? "Collapse All" : "Expand All", options);
+}
